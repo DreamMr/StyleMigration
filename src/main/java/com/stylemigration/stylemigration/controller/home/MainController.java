ec726f1e80b886b43d16f24e47bb380eec971dea
网之管 <qc_d434a8648c0e41ce9b5dc306a61ce3ae@git.code.tencent.com>
2019-05-12 20:14:07 +0800

    初创

---
+++ src/main/java/com/stylemigration/stylemigration/controller/home/MainController.java
@@ -0,0 +1,12 @@
+package com.stylemigration.stylemigration.controller.home;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping(value = "home")
+public class MainController {
+
+}
