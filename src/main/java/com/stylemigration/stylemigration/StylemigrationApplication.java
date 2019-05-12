785cd714ecdcd579ad1a630d669825876d5f442c
网之管 <qc_d434a8648c0e41ce9b5dc306a61ce3ae@git.code.tencent.com>
2019-05-12 20:05:51 +0800

    初创

---
+++ src/main/java/com/stylemigration/stylemigration/StylemigrationApplication.java
@@ -0,0 +1,13 @@
+package com.stylemigration.stylemigration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StylemigrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StylemigrationApplication.class, args);
+    }
+
+}
